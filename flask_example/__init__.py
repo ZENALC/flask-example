3b4cea05ab844d717cbb630a6543ec18ffb91fec
mshrestha <mihir.xtha@gmail.com>
2020-05-22 21:19:32 -0400

    added account configuration

--- flask_example/__init__.py
+++ flask_example/__init__.py
@@ -1,15 +1,15 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask import Flask  # import flask itself
+from flask_sqlalchemy import SQLAlchemy  # import the database package
+from flask_bcrypt import Bcrypt  # import encrypt package
+from flask_login import LoginManager  # import special login manager
 
-app = Flask(__name__)
+app = Flask(__name__)  # create Flask object
 app.config['SECRET_KEY'] = '015a3921e25e447c5bb42b0202101a94'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+db = SQLAlchemy(app)  # create database with Flask object
+bcrypt = Bcrypt(app)  # create encryption with Flask object
+login_manager = LoginManager(app)  # create login-manager with Flask object
+login_manager.login_view = 'login'  # the view for our login page
+login_manager.login_message_category = 'info'  # type of bootstrap class we'll use for messages
 
-from flask_example import routes
+from flask_example import routes  # import routes from flask_example package
--- flask_example/routes.py
+++ flask_example/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, url_for, flash, redirect, request
 from flask_example import app, db, bcrypt
-from flask_example.forms import RegistrationForm, LoginForm
+from flask_example.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_example.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
+from PIL import Image
 
 
 posts = [
@@ -24,20 +27,20 @@ posts = [
 
 @app.route('/home/')
 @app.route('/')
-def home():
+def home():  # view for home page
     return render_template('home.html', title='Home', posts=posts)
 
 
 @app.route('/about/')
-def about():
+def about():  # view for about page
     return render_template('about.html', title='About')
 
 
 @app.route('/register/', methods=['POST', 'GET'])
-def register():
-    if current_user.is_authenticated:
+def register():  # view for register page
+    if current_user.is_authenticated:  # if already logged in, redirect to home page
         return redirect(url_for('home'))
-    form = RegistrationForm()
+    form = RegistrationForm()  # get RegistrationForm from flask_example.forms
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
@@ -70,7 +73,35 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/account/')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route('/account/', methods=['POST', 'GET'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated.', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename=f'profile_pics/{current_user.image_file}')
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
